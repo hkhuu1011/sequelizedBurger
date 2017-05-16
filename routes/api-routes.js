1ff210dca1de4c452582ca7d905a5386068c27eb
Han Khuu <h.khuu01@gmail.com>
2017-05-16 18:25:51 -0400

    fixed devoured button

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,12 +3,6 @@ var db = require("../models");
 
 // Routes
 module.exports = function(app) {
-  // app.get("/", function(req, res) {
-  //   db.Burgers.findAll({}).then(function(dbBurgers) {
-  //     res.json(dbBurgers);
-  //   });
-  // });
-
   // GET route for getting all of the burgers
   app.get("/burgers", function(req, res) {
     // findAll returns all entries for a table when used with no options
@@ -30,12 +24,30 @@ module.exports = function(app) {
       // devoured: req.body.devoured
     }).then(function(dbBurgers) {
       // We have access to the new burger as an argument inside of the callback function
-      // res.json(dbBurgers);
       res.redirect("/");
     }).catch(function (error){
       res.json(error);
     });
   });
+
+  // PUT route for updating burgers. We can get the updated burger data from req.body
+  app.put("/burgers/:id", function(req, res) {
+    var condition = req.body.devoured;
+    console.log("devoured: ", req.body.devoured);
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Burgers.update({
+      // burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBurgers) {
+      res.redirect("/");
+    });
+  });
+
 
   // DELETE route for deleting todos. We can get the id of the todo to be deleted from
   // req.params.id
@@ -51,20 +63,5 @@ module.exports = function(app) {
 
   // });
 
-  // PUT route for updating burgers. We can get the updated burger data from req.body
-  app.put("/burgers", function(req, res) {
-    // Update takes in an object describing the properties we want to update, and
-    // we use where to describe which objects we want to update
-    db.Burgers.update({
-      // burger_name: req.body.burger_name,
-      devoured: true
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbBurgers) {
-      // res.json(dbBurgers);
-      res.redirect("/");
-    });
-  });
+
 };
